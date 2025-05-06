458279a4cad9ff644f82460f79c8d0982d50ffe7
JMBROWN207 <jmbrown207@gmail.com>
2025-05-06 18:05:43 +0100

    Update build.rs

--- build.rs
+++ build.rs
@@ -2,11 +2,9 @@ extern crate winres;
 
 fn main() {
     slint_build::compile("ui/ui.slint").expect("error compiling slint files");
-    
+
     #[cfg(target_os = "windows")]
-   
-        let mut res = winres::WindowsResource::new();
-        res.set_icon("og3-pack-updater-icon.ico");
-        res.compile().unwrap();
-      
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("og3-pack-updater-icon.ico");
+    res.compile().unwrap();
 }
